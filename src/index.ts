97997e251de2af6a1b190e516702561d245efd1c
Simon Fontaine <74004726+Simon-Fontaine@users.noreply.github.com>
2024-02-04 20:47:23 +0100

    created the base structure of the discord bot

---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Client, Collection, GatewayIntentBits, Partials } from 'discord.js';
+import { token, mongoURI } from '../config.json';
+import mongoose from 'mongoose';
+import fs from 'node:fs';
+import path from 'node:path';
+
+const client = new Client({
+	intents: [
+		GatewayIntentBits.Guilds,
+		GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.MessageContent,
+	],
+	partials: [Partials.Channel, Partials.GuildMember, Partials.User],
+});
+
+client.commands = new Collection();
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs
+		.readdirSync(commandsPath)
+		.filter((file) => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(
+				`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`
+			);
+		}
+	}
+}
+
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs
+	.readdirSync(eventsPath)
+	.filter((file) => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+(async () => {
+	try {
+		await mongoose.connect(mongoURI);
+		console.log('Connected to MongoDB');
+	} catch (error) {
+		console.error(error);
+	}
+})();
+
+client.login(token);
